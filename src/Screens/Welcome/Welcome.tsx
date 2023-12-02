5481540b2d409db35ba757117d7f2e45b78b8ccd
Phạm Duy Quang <75131380+ULTIMATE-Mystery@users.noreply.github.com>
2023-12-03 05:07:34 +0700

    Add font Montserrat, finish On Board screen

--- src/Screens/Welcome/Welcome.tsx
+++ src/Screens/Welcome/Welcome.tsx
@@ -1,20 +1,49 @@
 import React from "react";
 import { i18n, LocalizationKey } from "@/Localization";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import { StatusBar } from "expo-status-bar";
-import { Button } from "native-base";
 import { RootScreens } from "..";
+import { LinearGradient } from 'expo-linear-gradient';
+import { Ionicons } from '@expo/vector-icons';
 
 export const Welcome = (props: {
   onNavigate: (string: RootScreens) => void;
 }) => {
   return (
     <View style={styles.container}>
-      <Text>{i18n.t(LocalizationKey.WELCOME)}</Text>
-      <StatusBar style="auto" />
-      <Button onPress={() => props.onNavigate(RootScreens.MAIN)}>
-        {i18n.t(LocalizationKey.START)}
-      </Button>
+      <LinearGradient
+        colors={['#1488D8', '#1488D8', '#FFFFFF']}
+        style={styles.container}
+      >
+        <StatusBar barStyle="light-content" />
+        <Image source={require('../../../assets/image/maproute.png')} style={styles.illustration1} />
+        <View style={styles.logoContainer}>
+          <Image source={require('../../../assets/image/Logo.png')} style={styles.logo} />
+        </View>
+
+        <View style={styles.textContainer}>
+          <View style={styles.shadow}>
+            <Text style={styles.greeting}>Chào bạn!</Text>
+          </View>
+          <View style={styles.shadow}>
+            <Text style={styles.description}>
+              Chúng tôi là <Text style={styles.boldText}>BKWAI</Text> - Ứng dụng tìm kiếm thông tin thông qua mã QR
+            </Text>
+          </View>
+          <View style={styles.shadow}>
+            <Text style={styles.slogan}>Quét để khám phá - Bản đồ khuôn viên trường chỉ qua một lần chạm</Text>
+          </View>
+        </View>
+
+        <Image source={require('../../../assets/image/personwithmap.png')} style={styles.illustration2} />
+        <TouchableOpacity
+          style={[styles.button, styles.shadow]}
+          onPress={() => props.onNavigate(RootScreens.MAIN)}
+        >
+        <Text style={styles.buttonText}>Bắt đầu ngay</Text>
+        <Ionicons name="arrow-forward" size={45} color="#1488D8" />
+        </TouchableOpacity>
+      </LinearGradient>
     </View>
   );
 };
@@ -22,8 +51,94 @@ export const Welcome = (props: {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: 'black',
+  },
+  illustration1: {
+    marginTop: '11%',
+    marginLeft: "-17%",
+    position: 'absolute',
+    width: 300, // Set the width as needed.
+    height: '25%', // Set the height as needed.
+    resizeMode: 'contain',   
+  },
+  logoContainer: {
+    marginTop: '20%',
+    width: '100%', // Takes the full width of the screen
+    alignItems: 'center', // Centers the logo horizontally
+  },
+  textContainer: {
+    marginTop: '10%',
+    paddingHorizontal: 25, // Adjust padding as needed
+    width: '100%', // Ensures the container takes the full width
+  },
+  greeting: {
+    fontSize: 32,
+    color: '#fff',
+    marginBottom: '4%',
+    textAlign: 'justify',
+    fontFamily: 'Montserrat-Bold',
+  },
+  description: {
+    fontSize: 18,
+    color: '#fff',
+    textAlign: 'justify', // Justify the description text
+    marginBottom: 20,
+    fontFamily: 'Montserrat-Regular',
+  },
+  slogan: {
+    fontSize: 23,
+    color: '#fff',
+    textAlign: 'justify', 
+    fontFamily: 'Montserrat-Bold',
+  },
+  illustration2: {
+    width: '90%', // Take the full width of the screen
+    height: '40%', // Fixed height
+    resizeMode: 'contain',
+    position: 'absolute', // Position absolutely to take it out of the flow
+    bottom: '1%', // Adjust this to place the image vertically where you want it
+    left: '20%', // Start from the left edge
+    alignSelf: 'center', // Center the image in the available space
+  },
+  button: {
+    backgroundColor: '#fff',
+    paddingTop: 15,
+    paddingBottom: 15,
+    paddingLeft: 35,
+    paddingRight: 35,
+    borderRadius: 45,
+    bottom: '8%',
+    position: 'absolute',
+    flexDirection: 'row',
+    alignItems: 'center', // Align items in the button
+    justifyContent: 'center', // Center button content
+    borderColor: '#1488D8',
+    borderWidth: 2,
+    alignSelf: 'center', // Center the button in the container
+  },
+
+  buttonText: {
+    fontSize: 30,
+    color: 'black', // Adjust text color to match your design.
+    fontFamily: 'Montserrat-Bold',
+    marginRight: 8,
+  },
+
+  shadow: {
+    ...Platform.select({
+      ios: {
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+      },
+      android: {
+        elevation: 5,
+      },
+    }),
+  },
+
+  boldText: {
+    fontWeight: '900',
   },
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,29 @@
 import React from "react";
 import * as Localization from "expo-localization";
+import AppLoading from 'expo-app-loading';
 import { i18n, Language } from "@/Localization";
 import { NativeBaseProvider } from "native-base";
 import { store, persistor } from "@/Store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { ApplicationNavigator } from "./Navigation";
+import {useFonts} from 'expo-font';
 
 i18n.locale = Localization.locale;
 i18n.enableFallback = true;
 i18n.defaultLocale = Language.ENGLISH;
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    'Montserrat-Regular': require('../assets/font/static/Montserrat-Regular.ttf'),
+    'Montserrat-Bold': require('../assets/font/static/Montserrat-Bold.ttf'),
+    'Montserrat-ExtraBold': require('../assets/font/static/Montserrat-ExtraBold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+  
   return (
     <NativeBaseProvider>
       <Provider store={store}>
